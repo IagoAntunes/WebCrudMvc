b75a87eee5bad0940e214b4e9964abf898beaf30
iagoa <iagoantunes.f@gmail.com>
2022-06-12 00:56:39 -0300

    Adicionar arquivos de projeto.

---
+++ WebCRUDMVCSQL/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebCRUDMVCSQL.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }//Falar pro Entity quais modelos que ele gerencie
+    }
+}
---
+++ WebCRUDMVCSQL/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCRUDMVCSQL.Models
+{
+    [Table("Produto")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "Code")]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+    }
+}
